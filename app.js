6c1f5acd0610523bb008b76182acdda15082c3ac
bp-pepper <53023606+bp-pepper@users.noreply.github.com>
2024-02-19 21:33:24 -0500

    init setup, add dir/structure, app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+const bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    express = require("express"),
+    app = express(),
+    session = require("express-session"),
+    methodOverride = require("method-override"),
+    flash = require("connect-flash");
+
+mongoose.connect("mongodb://localhost:27017/port_projects", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.use(flash());
+
+app.use("/", require("./routes/index"));
+app.use("/projects", require("./routes/projects"));
+
+app.listen(3000, () => {
+    console.log("brentpepper.dev is up and running!!");
+});
